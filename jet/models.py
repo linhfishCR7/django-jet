55433c012fc158ca7b15bd7ba143e33b83c03f22
linhfishCR7 <fish@devx.com.au>
2022-10-31 18:52:34 +0700

    fixed uuid user model

--- jet/models.py
+++ jet/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.utils import timezone
 try:
     from django.utils.translation import ugettext_lazy as _
@@ -14,7 +15,7 @@ except ImportError:
 class Bookmark(models.Model):
     url = models.URLField(verbose_name=_('URL'))
     title = models.CharField(verbose_name=_('title'), max_length=255)
-    user = models.PositiveIntegerField(verbose_name=_('user'))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('user'), on_delete=models.CASCADE)
     date_add = models.DateTimeField(verbose_name=_('date created'), default=timezone.now)
 
     class Meta:
@@ -29,7 +30,7 @@ class Bookmark(models.Model):
 @python_2_unicode_compatible
 class PinnedApplication(models.Model):
     app_label = models.CharField(verbose_name=_('application name'), max_length=255)
-    user = models.PositiveIntegerField(verbose_name=_('user'))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('user'), on_delete=models.CASCADE)
     date_add = models.DateTimeField(verbose_name=_('date created'), default=timezone.now)
 
     class Meta:
